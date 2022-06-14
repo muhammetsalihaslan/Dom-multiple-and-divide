59df5595c2284422eb3890beda64fec589b44c60
salih <muhammetsalihaslan@gmail.com>
2022-06-14 17:18:18 +0300

    up3

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("*******App js*********");
+
+const Number1 = document.getElementById("number1");
+const Number2 = document.getElementById("number2");
+
+const Result = document.getElementById("result");
