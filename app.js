1bc83f62d03e80821afed2f3f19aaf1cc780f9e8
salih <muhammetsalihaslan@gmail.com>
2022-06-14 19:03:59 +0300

    up4

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const Number1 = document.getElementById("number1");
 const Number2 = document.getElementById("number2");
 
 const Result = document.getElementById("result");
+
+function multiplyBy() {
+   num1 = Number1.value;
+   num2 = Number2.value;
+    Result.innerHTML = num1 * num2;
+}
+
+function divideBy() {
+    num1 = Number1.value;
+    num2 = Number2.value;
+    Result.innerHTML = num1 / num2;
+}
